75a267a15b96e4ffd6b4038d56bac04c21d9044d
Mesenev Pavel <heredemonsnearby@mail.ru>
2024-05-07 21:21:33 +1000

    add characteristic to syn

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,1 +1,2 @@
 docker run --rm -it -v $(pwd):/source -w /source aergus/latex latexmk -bibtex -pdf dissertation.tex
+docker run --rm -it -v $(pwd):/source -w /source aergus/latex latexmk -bibtex -pdf synopsis.tex
