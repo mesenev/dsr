79ededfc8c43661a0ff1f0956248a88612541185
Mesenev Pavel <heredemonsnearby@mail.ru>
2024-10-31 21:58:31 +1000

    Presentation [work in progress]

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,3 +1,3 @@
 docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source aergus/latex latexmk -bibtex -pdf dissertation.tex
 docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source aergus/latex latexmk -bibtex -pdf synopsis.tex
-#docker run --rm -it -v $(pwd):/source -w /source aergus/latex latexmk -bibtex -pdf presentation_handout.tex
+docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source aergus/latex latexmk -pdf presentation.tex
