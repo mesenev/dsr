2b3e9547fa7baebe41d1691bf10c074d0c089c4c
Pavel Mesenev <mesenev_pr@dvfu.ru>
2025-10-27 18:24:51 +1000

    final version dis\syn

--- build_pdf.sh
+++ build_pdf.sh
@@ -3,10 +3,12 @@
 rm *.fls *.out *.bcf *.bbl *.aux *.pdf *.blg *.fdb_latexmk *.log *.run.xml
 docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source raabf/texstudio-versions latexmk -bibtex -pdf synopsis.tex
 docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source raabf/texstudio-versions latexmk -bibtex -pdf synopsis.tex
+cp --force synopsis.pdf syn_$(date +%d)$(date +%b).pdf
 
 
 ## dissertation
-
-#rm *.fls *.out *.bcf *.bbl *.aux *.blg *.fdb_latexmk *.log *.run.xml
+#
+##rm *.fls *.out *.bcf *.bbl *.aux *.blg *.fdb_latexmk *.log *.run.xml
 #docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source raabf/texstudio-versions latexmk -bibtex -pdf dissertation.tex
 #docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source raabf/texstudio-versions latexmk -bibtex -pdf dissertation.tex
+#cp --force dissertation.pdf diss_$(date +%d)$(date +%b).pdf
