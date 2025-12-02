601832df5a865b06247924a48a787b2d53956670
Pavel Mesenev <mesenev_pr@dvfu.ru>
2025-12-02 10:18:32 +1000

    printed.

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,14 +1,17 @@
 ## synopsis
 
-rm *.fls *.out *.bcf *.bbl *.aux *.pdf *.blg *.fdb_latexmk *.log *.run.xml
-docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source raabf/texstudio-versions latexmk -bibtex -pdf synopsis.tex
-docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source raabf/texstudio-versions latexmk -bibtex -pdf synopsis.tex
-cp --force synopsis.pdf syn_$(date +%d)$(date +%b).pdf
+#rm *.fls *.out *.bcf *.bbl *.aux *.pdf *.blg *.fdb_latexmk *.log *.run.xml
+#docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source raabf/texstudio-versions latexmk -bibtex -pdf synopsis.tex
+#docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source raabf/texstudio-versions latexmk -bibtex -pdf synopsis.tex
+#cp --force synopsis.pdf syn_$(date +%d)$(date +%b).pdf
 
 
 ## dissertation
 #
-##rm *.fls *.out *.bcf *.bbl *.aux *.blg *.fdb_latexmk *.log *.run.xml
-#docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source raabf/texstudio-versions latexmk -bibtex -pdf dissertation.tex
-#docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source raabf/texstudio-versions latexmk -bibtex -pdf dissertation.tex
-#cp --force dissertation.pdf diss_$(date +%d)$(date +%b).pdf
+#rm *.fls *.out *.bcf *.bbl *.aux *.blg *.fdb_latexmk *.log *.run.xml
+docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source raabf/texstudio-versions latexmk -bibtex -pdf dissertation.tex
+docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source raabf/texstudio-versions latexmk -bibtex -pdf dissertation.tex
+cp --force dissertation.pdf diss_$(date +%d)$(date +%b).pdf
+
+#docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source raabf/texstudio-versions latexmk -bibtex -pdf presentation.tex
+#cp --force presentation.pdf pres_$(date +%d)$(date +%b).pdf
