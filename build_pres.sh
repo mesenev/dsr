b2b728154ba9fccd924fded525d95cb4d4e0c765
Pavel Mesenev <mesenev_pr@dvfu.ru>
2026-02-10 19:52:27 +1000

    opponents answers pres

--- build_pres.sh
+++ build_pres.sh
@@ -1,2 +1,3 @@
 docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source aergus/latex latexmk -pdf -f presentation.tex
+rm *.fls *.out *.bcf *.bbl *.aux *.blg *.fdb_latexmk *.log *.run.xml
 docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source aergus/latex latexmk -pdf -f presentation_notes.tex
