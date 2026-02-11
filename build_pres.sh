630af4525c3f0f6d732ddb10f2968e974aa4a9ec
Pavel Mesenev <mesenev_pr@dvfu.ru>
2026-02-11 22:38:12 +1000

    pres notes to 12th slide

--- build_pres.sh
+++ build_pres.sh
@@ -1,3 +1,3 @@
-docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source aergus/latex latexmk -pdf -f presentation.tex
+#docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source aergus/latex latexmk -pdf -f presentation.tex
 rm *.fls *.out *.bcf *.bbl *.aux *.blg *.fdb_latexmk *.log *.run.xml
 docker run -u $(id -u):$(id -g) --rm -it -v $(pwd):/source -w /source aergus/latex latexmk -pdf -f presentation_notes.tex
